251a6381b0f9aa1d98b8f59110bfdaea4793d775
Chou Yun <chowduen@gmail.com>
2021-01-04 12:13:51 +0800

    testing icon

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ L.marker(
     ).addTo(map);
 
 var redIcon = new L.Icon({
-    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     iconSize: [25, 41],
     iconAnchor: [12, 41],
